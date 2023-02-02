4048c6c6f740ab90caa73c3f9b49dd915561f872
Micah Halter <micah@mehalter.com>
2023-02-02 08:59:07 -0500

    add spectre search/replace

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -66,6 +66,25 @@ return {
       lastplace_open_folds = true,
     },
   },
+  {
+    "mehalter/nvim-spectre", -- move to main repo after https://github.com/nvim-pack/nvim-spectre/pull/104
+    -- "nvim-pack/nvim-spectre",
+    cmd = "Spectre",
+    opts = function()
+      local prefix = "<leader>s"
+      return {
+        mapping = {
+          send_to_qf = { map = prefix .. "q" },
+          replace_cmd = { map = prefix .. "c" },
+          show_option_menu = { map = prefix .. "o" },
+          run_current_replace = { map = prefix .. "C" },
+          run_replace = { map = prefix .. "R" },
+          change_view_mode = { map = prefix .. "v" },
+          resume_last_search = { map = prefix .. "l" },
+        },
+      }
+    end,
+  },
   { "junegunn/vim-easy-align", init = function() table.insert(astronvim.file_plugins, "vim-easy-align") end },
   { "machakann/vim-sandwich", init = function() table.insert(astronvim.file_plugins, "vim-sandwich") end },
   { "wakatime/vim-wakatime", init = function() table.insert(astronvim.file_plugins, "vim-wakatime") end },
