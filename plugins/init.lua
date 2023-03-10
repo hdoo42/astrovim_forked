1ea518064d67509958f557ea7a110bb71f731205
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-03-10 22:17:42 +0900

    add pets

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,11 +1,21 @@
 return {
   { "42Paris/42header", lazy = false },
-  { "eandrju/cellular-automaton.nvim" },
   { "github/copilot.vim", lazy = false },
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
       require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } }
+    end,
+  },
+  {
+    "giusgad/pets.nvim",
+    lazy = false,
+    dependencies = { "MunifTanjim/nui.nvim", "giusgad/hologram.nvim" },
+    config = function()
+      require("pets").setup {
+        row = 6,
+        default_pet = "dog",
+      }
     end,
   },
 }
