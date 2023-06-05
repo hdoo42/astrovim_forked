104e5a40197d34dc7cf2cb9e46b8324d6b4b38eb
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-06-05 15:38:43 +0900

    remove ensure installed

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -3,18 +3,13 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = {
-      },
+      ensure_installed = {},
     },
   },
   {
     "jay-babu/mason-null-ls.nvim",
     opts = {
-      ensure_installed = {
-        "css-lsp",
-        "html-lsp",
-        "prettierd",
-      },
+      ensure_installed = {},
       handlers = {
         taplo = function() end, -- disable taplo in null-ls, it's taken care of by lspconfig
       },
