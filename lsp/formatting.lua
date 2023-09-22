831ef9ee12e94d2578d200be049ab3e44d449d30
Hyeokjin Doo <hdoo@c1r2s4.42seoul.kr>
2023-09-22 20:50:38 +0900

    disable c formatting

--- lsp/formatting.lua
+++ lsp/formatting.lua
@@ -1,9 +1,6 @@
 return {
   format_on_save = {
     enabled = true, -- enable format on save
-    ignore_filetypes = { -- disable format on save for specified filetypes
-      "c",
-    },
   },
   disabled = {
     "lua_ls",
