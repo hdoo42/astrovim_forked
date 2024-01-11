fb7a6c6b63474887927fd9799c02aaeb9f8b929c
Hyeokjin Doo <hdoo@c1r2s4.42seoul.kr>
2024-01-11 10:00:19 +0900

    luasnip

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,4 +1,15 @@
 return {
-  "ggandor/leap.nvim",
-  lazy = false,
+  { "ggandor/leap.nvim", lazy = false },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function(plugin, opts)
+      -- include the default astronvim config that calls the setup call
+      require "plugins.configs.luasnip"(plugin, opts)
+      -- load snippets paths
+      require("luasnip.loaders.from_vscode").lazy_load {
+        paths = { "~/.config/nvim/lua/user/snippets" },
+      }
+    end,
+  },
 }
