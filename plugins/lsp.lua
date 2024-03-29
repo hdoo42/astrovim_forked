f3c82214443fe53d0e8fd14312abe2dfd1b102d9
Hyeokjin <tyeirdoo06@gmail.com>
2024-03-29 14:39:00 +0900

    disable require tsserver for ts file

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -3,6 +3,6 @@ return {
     "jose-elias-alvarez/typescript.nvim",
     init = function() table.insert(astronvim.lsp.skip_setup, "tsserver") end,
     ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-    opts = function() return { server = require("astronvim.utils.lsp").config "tsserver" } end,
+    -- opts = function() return { server = require("astronvim.utils.lsp").config "tsserver" } end,
   },
 }
